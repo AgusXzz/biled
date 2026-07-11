1fd9a41cd101ab149cbd3c18673bd1f297abe52f
AgusXzz <davidmersey707@gmail.com>
2026-07-11 20:46:31 +0800

    fix: GenerateStatics.sh cd to own dir and proto-extract path

--- WAProto/GenerateStatics.sh
+++ WAProto/GenerateStatics.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+cd "$(dirname "$0")"
 sed -i 's/required /optional /g' WAProto.proto
-pbjs -t static-module --no-beautify -w es6 --no-bundle --no-delimited --no-verify --no-comments -o ./index.js ./WAProto.proto;
+npx pbjs -t static-module --no-beautify -w es6 --no-bundle --no-delimited --no-verify --no-comments -o ./index.js ./WAProto.proto
 node ./fix-imports.js
